43125ac831d569a4894ef001924842d5e969db8e
事实 <14741157354@163.com>
2018-08-16 17:20:01 +0800

    Add files via upload

---
+++ SSAVplayerDemo/BaseVC/BaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  BaseViewController.h
+//  SSAVplayerDemo
+//
+//  Created by 李鹏 on 2018/8/16.
+//  Copyright © 2018年 lipeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseViewController : UIViewController
+
+@end
---
+++ SSAVplayerDemo/BaseVC/BaseViewController.m
@@ -0,0 +1,39 @@
+//
+//  BaseViewController.m
+//  SSAVplayerDemo
+//
+//  Created by 李鹏 on 2018/8/16.
+//  Copyright © 2018年 lipeng. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface BaseViewController ()
+
+@end
+
+@implementation BaseViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor whiteColor];
+    NSLog(@"我叫123");
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
